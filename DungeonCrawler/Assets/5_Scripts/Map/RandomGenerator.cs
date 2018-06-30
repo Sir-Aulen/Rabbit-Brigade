dd0482d34f2f0615bfc2fd4421f77ef69668f676
Adam Gallina <adamg461@gmail.com>
2018-06-30 09:00:26 -0700

    Set up game

---
+++ DungeonCrawler/Assets/5_Scripts/Map/RandomGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomGenerator : MonoBehaviour {
+
+    public List<List<int>> map = new List<List<int>>();
+
+    string row;
+
+    private void Start()
+    {
+        newMap(5, 3);
+    }
+
+    public void newMap(int height, int length)      //0: Wall
+    {                                               //1: Corridor
+        map.Clear();
+
+        for (int i = 0; i < height; i++)
+        {
+            map.Add(new List<int>());
+            for (int ii = 0; ii < length; ii++) map[i].Add(0);
+        }
+        
+        for (int i = 0; i < height; i++)
+        {
+            for (int ii = 0; ii < length; ii++) row += map[i][ii].ToString();
+            Debug.Log(row);
+            row = "";
+        }
+    }
+}
---
+++ DungeonCrawler/Assets/5_Scripts/Player/PlayerMvmt.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMvmt : MonoBehaviour {
+    
+    private float x = 0;
+    private float y = 0;
+    public float speed = 5;
+    private Vector3 dir;
+    private Vector3 target;
+
+	void Start ()
+    {
+	}
+	
+	void Update ()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        target = new Vector3(transform.position.x + x, transform.position.y + y, 0);
+        dir = (target - transform.position).normalized;
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
